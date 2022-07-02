f3e2ca54e2ebc6dc94fbfc6497468cc6fc31aa5a
henquesz <henquesvinicius@gmail.com>
2022-07-02 17:31:15 -0300

    inclusão do modo escuro nas páginas

--- js/darkMode.js
+++ js/darkMode.js
@@ -3,3 +3,8 @@ const btn = document.getElementById("btn");
 btn.addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
 });
+
+function darkmode(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
