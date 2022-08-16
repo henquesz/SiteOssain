ef466b99409f5187a7d3699ee31d1422f5af1f79
henquesz <henquesvinicius@gmail.com>
2022-08-16 16:36:37 -0300

    inclusão do darkmode funcional

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,10 +1,29 @@
-const btn = document.getElementById("btn");
+window.onload=function(){
 
-btn.addEventListener("change", (e) => {
-    document.body.classList.toggle("dark", e.target.checked);
-});
+    const darnkBtn = document.getElementById('teste');
+    const bodyEl = document.querySelector('body');
+    
+    const darkMode = () => {
+        bodyEl.classList.toggle('dark-mode')
+    } 
 
-function darkmode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    darnkBtn.addEventListener('click', () => {
+        setDarkMode = localStorage.getItem('dark-mode');
+
+        if(setDarkMode !== "on"){
+            darkMode();
+
+            setDarkMode = localStorage.setItem('dark-mode', 'on');
+        }else{
+            darkMode();
+
+            setDarkMode = localStorage.setItem('dark-mode', null);
+        }
+    });
+
+    let setDarkMode = localStorage.getItem('dark-mode');
+
+    if(setDarkMode === 'on'){
+        darkMode();
+    }
 }
